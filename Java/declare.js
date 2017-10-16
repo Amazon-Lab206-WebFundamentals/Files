898077c7b13ed3a3867d62e8d11c003a2aa9c6c1
Hendry <noahendr@amazon.com>
2017-10-16 11:48:54 -0700

    declare.js

---
+++ Java/declare.js
@@ -0,0 +1,22 @@
+var num1 = 5;
+var num2 = 44;
+var num3 = -6;
+var num4 = .64;
+var string1 = "Noah";
+var string2 = "Hendry";
+var string3 = "Lab 206";
+var boolean1 = true;
+var boolean2 = false;
+
+console.log(num1, " + ", num2, " + ", num3, " + ", num4, "=", num1+num2+num3+num4);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log("My First String ",string1);
+console.log("My Second String ",string2);
+console.log("My Third String ",string3);
+
+console.log("Coding dojo is fun! === ",boolean1);
+console.log("Lab 206 is lame?",boolean2);
+
+
